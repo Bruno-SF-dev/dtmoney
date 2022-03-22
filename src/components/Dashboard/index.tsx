37e8df264d51885baf2b605f237cad9bb62f2d14
Bruno Santos <brunosantosfarias2@gmail.com>
2022-03-21 22:29:01 -0300

    estilização do Summary

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Summary } from "../Summary";
+import * as Styled from "./styles";
+
+const Dashboard = () => {
+  return (
+    <Styled.Container>
+      <Summary />
+    </Styled.Container>
+  );
+};
+
+export { Dashboard };
