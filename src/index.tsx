0098dd88031c0d1597d9be0da174efd8d28d4c40
Bruno Santos <brunosantosfarias2@gmail.com>
2022-03-23 00:06:54 -0300

    buscar dados na api

--- src/index.tsx
+++ src/index.tsx
@@ -8,16 +8,36 @@ createServer({
     transaction: Model,
   },
 
-    this.get("/transactions", () => {
-      return [
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
         {
           id: 1,
-          title: "Transaction 1",
-          amount: 400,
+          title: "Freelance de website",
           type: "deposit",
-          category: "Food",
-          createdAt: new Date(),
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
         },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1200,
+          createdAt: new Date("2021-02-14 11:00:00"),
+        },
+      ],
+    });
+  },
+
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return this.schema.all("transaction");
+    });
+
     this.post("/transactions", (schema, request) => {
       const data = JSON.parse(request.requestBody);
 
