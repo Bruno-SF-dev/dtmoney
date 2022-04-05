797c126932ed64be5aedef03c9a21565120c5d69
Bruno Santos <brunosantosfarias2@gmail.com>
2022-04-05 17:33:42 -0300

    commit test

--- src/components/FormCreateUser/index.tsx
+++ src/components/FormCreateUser/index.tsx
@@ -4,6 +4,8 @@ import { AuthContext } from "../../contexts/authContext";
 
 import * as Styled from "./styles";
 
+//
+
 export function FormCreateUser() {
   const navigate = useNavigate();
   const { onCreateUser } = useContext(AuthContext);
